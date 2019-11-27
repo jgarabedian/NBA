9ad8cd1d72ea8d409584f58659c70c5858eedf77
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-11-27 09:22:36 -0500

    initial commit for mpg visual

---
+++ assets/js/mpg.js
@@ -0,0 +1,14 @@
+// Initialize
+var mpgChart = echarts.init(document.getElementById('mpg'));
+
+// testData
+var testData = {
+    'Jrue Holiday': {
+        'min': '34:08'
+    },
+    'Mike Conley': {
+        'min': '30:33'
+    }
+}
+
+var maxTime = '48:00';
