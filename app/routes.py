f86fb15cc8373b6c91cf11c22510dc63977e7071
Jack Garabedian <33696930+jgarabedian@users.noreply.github.com>
2019-12-23 14:52:52 -0500

    file restructure, add header template

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,18 @@ from app.scripts import *
 
 
 @app.route('/')
+def Home():
+    title = 'Home'
+    return render_template('index.html.j2', title = title)
+
 @app.route('/players')
 def index():
-    user = {'username': 'Jack'}
-    return render_template('players/players.html.j2')
+    title = 'Players'
+    return render_template('players/players.html.j2', title = title)
 
 @app.route('/betting')
 def betting():
     matchupList = matchups.getMatchups()
     oddsList = spreads.getSpreads()
-    return render_template('betting/betting.html.j2', matchups = matchupList, odds = oddsList)
+    title = 'Betting'
+    return render_template('betting/betting.html.j2', matchups = matchupList, odds = oddsList, title = title)
