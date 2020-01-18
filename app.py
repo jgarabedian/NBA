bcad4bbb7fa6c7d656a951f905617fa858433e83
Jack Garabedian <jgarabedian96@gmail.com>
2020-01-18 10:51:34 -0500

    add celery configs

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from app import app
+from celery import Celery
+
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 # set to false when deploying to prod
-app.debug = False
+app.debug = True
 if __name__ == '__main__':
    app.run()
