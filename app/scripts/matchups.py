58c62ed56f53d6c96b71870cc04acf178704f135
Jack Garabedian <jgarabedian96@gmail.com>
2020-01-26 14:13:12 -0500

    fix for new html tags in oddshark

--- app/scripts/matchups.py
+++ app/scripts/matchups.py
@@ -19,21 +19,51 @@ def getMatchups():
 
     # regex search to find the matchups
     regex = re.compile('op-matchup-wrapper basketball')
-    matchups_list = soup.find_all('div', attrs={'class': regex})
+    regex2 = re.compile('op-matchup-links has-matchup-links')
+    matchups_list = soup.find_all('div', attrs={'class': [regex, regex2]})
+    # print(matchups_list)
     matchTeamList = []
     for match in matchups_list:
-        matchTime = match.find('div', {'class': 'op-matchup-time op-matchup-text'}).getText()
-        matchTop = match.find('div', {'class': 'op-matchup-team op-matchup-text op-team-top'}).getText()
-        matchBottom = match.find('div', {'class': 'op-matchup-team op-matchup-text op-team-bottom'}).getText()
-        matchDate = match.find('a', {'class': 'base-versus'})['href']
+        # print(match)
+        # matchTime = match.find('div', {'class': 'op-matchup-time op-matchup-text'}).getText()
+        cnt = 0
+        for a in match:
+            if len(match) == 3:
+                if cnt == 0:
+                    # print(a.text)
+                    matchTime = a.text
+                    # cnt += 1
+                    # continue
+                elif cnt == 2:
+                    # print(a)
+                    matchTop = a.find('a', {'class': 'odds-link op-matchup-team-text'}).text
+                    matchBottom = a.find('div', {'class': 'op-matchup-team op-matchup-text op-team-bottom'}).text
+                    # cnt += 1
+                    break
+                cnt += 1
+            # matchTop = a.find('a', {'class': 'odds-link op-matchup-team-text'})
+            # if matchTop != None:
+                # break
+        # matchTop = match.find('a', {'class': 'odds-link op-matchup-team-text'})
+        # matchTop = 'String'
+        # print(matchTop)
+        # matchBottom = match.find('div', {'class': 'op-matchup-team op-matchup-text op-team-bottom'}).getText()
+        # matchDate = match.find('a', {'class': 'odds-link full-matchup'})['href']
+        # print(matchDate)
         # parse and format the date
-        matchDate = getMatchDate(matchDate)
+        # matchDate = getMatchDate(matchDate)
+        # matchTime = 'TBD'
+        matchDate = 'TBD'
+        # matchBottom = 'TBD'
 
         matchVs = str(matchDate) + ' ' + str(matchTime) + ': ' + str(matchTop) + ' vs ' + str(matchBottom)
+        # print(matchVs)
+        # matchVs = str(matchTime) + ': ' + str(matchTop) + ' vs ' + str(matchBottom)
 
         # print(matchDate)
-        matchTeamList.append(matchVs)
-
+        if matchVs not in matchTeamList:
+            matchTeamList.append(matchVs)
+    print(matchTeamList)
     return matchTeamList
 
 
@@ -49,3 +79,4 @@ def getMatchDate(string):
     string = dparser.parse(str(string), fuzzy=True).strftime('%B %d %Y')
 
     return string
+# getMatchups()
